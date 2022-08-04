876d45620d0ca3087e93cc6639cb283286991d94
zain gillani <abdullahgillaniv204@gmail.com>
2022-08-04 19:05:57 +0500

    ok

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,12 +14,13 @@ class UserController extends Controller
         $user= User::where(['email' => $req->email])->first();
         //this check function will compare my form password to my database pasword
         //and the secondly we will check that if the user data is null
-        if(Hash::check($req->password,$user->password || !$user)){
+        if(!$user || Hash::check($req->password,$user->password)==false){
 
             return "username and password is incorrect";
         }else{
                 $req->session()->put("user",$user);
-            return redirect('/');
+            // return redirect('/');
+            return "login sucessfull";
         }
     }
 }
---
+++ app/Http/Middleware/UserAuth.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class UserAuth
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    { //here i compare incoming request path if it is equal to login and request session has user data its mean we are already exists or user already exists
+       // so we redirect to home page
+        if($request->path()=="loginpanel" && $request->session()->has('user')){
+
+            return redirect('/');
+        }
+        return $next($request);
+    }
+}
